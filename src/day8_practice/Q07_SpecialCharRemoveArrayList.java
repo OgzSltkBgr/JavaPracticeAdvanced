7f0d7f200ce40cd824b7e2596ec9367421a74eb4
OgzSltkBgr <105450835+OgzSltkBgr@users.noreply.github.com>
2022-07-24 21:39:59 +0300

    23.07.22

--- src/day8_practice/Q07_SpecialCharRemoveArrayList.java
+++ src/day8_practice/Q07_SpecialCharRemoveArrayList.java
@@ -1,5 +1,9 @@
 package day8_practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Q07_SpecialCharRemoveArrayList {
     public static void main(String[] args) {
         /*
@@ -15,6 +19,27 @@ public class Q07_SpecialCharRemoveArrayList {
    ArrayList= $-13, $0, $0
    Cevap = -1 olmalı.
     */
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("$13");
+        arrayList.add("$15");
+        arrayList.add("$20");
 
+        getSum(arrayList);
+        System.out.println(arrayList); //[$13, $15, $20]
+
+        System.out.println(getSum(arrayList));
+    }
+
+    private static int getSum(List<String> arrayList) {
+        int toplam = 0;
+        for (String each : arrayList
+        ) {
+            Integer sayi = Integer.parseInt(each.replaceAll("\\$",""));
+            toplam += sayi;
+        }
+        if(toplam >=0){
+            return toplam;
+        }else
+            return -1;
     }
 }
--- src/day8_practice/Q08_UniqeArray.java
+++ src/day8_practice/Q08_UniqeArray.java
@@ -1,5 +1,8 @@
 package day8_practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q08_UniqeArray {
     public static void main(String[] args) {
         /*
@@ -9,5 +12,14 @@ public class Q08_UniqeArray {
          * INPUT : {1,2,2,3,1,4,2,-2,5,6,8,7,5,6,-2,9,3,10}
          * OUTPUT : [1, 2, 3, 4, -2, 5, 6, 8, 7, 9, 10]
          */
+        int arr[]= {1,2,2,3,1,4,2,-2,5,6,8,7,5,6,-2,9,3,10};
+        List<Integer> list= new ArrayList<>();
+        for (int i = 0; i < arr.length ; i++) {
+               if(!list.contains(arr[i])){
+                   list.add(arr[i]);
+               }
+
+        }
+        System.out.println(list);
     }
 }
